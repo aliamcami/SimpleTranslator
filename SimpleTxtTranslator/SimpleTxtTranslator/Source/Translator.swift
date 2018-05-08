7b63913019cc76f8f9d4a902b7612cf9180cfaf6
Camila Resende <aliamcami@gmail.com>
2018-05-08 13:30:28 -0300

    Add translate extension to String

---
+++ SimpleTxtTranslator/SimpleTxtTranslator/Source/Translator.swift
@@ -0,0 +1,31 @@
+//
+//  Translator.swift
+//  SimpleTxtTranslator
+//
+//  Created by camila oliveira on 08/05/18.
+//  Copyright © 2018 Aliamcami. All rights reserved.
+//
+
+import UIKit
+
+public extension String{
+    public func translate(from sourceLang: Languages = .Automatic, to targetLang: Languages, onCompletion: @escaping (String)->()){
+        let sourceText = addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        let url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=\(sourceLang.rawValue)&tl=\(targetLang.rawValue)&dt=t&q=\(sourceText ?? "")"
+        
+        
+        let request = NSMutableURLRequest(url: URL.init(string: url)!)
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, err -> Void in
+            do{
+                let obj = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as AnyObject
+                let arr = (obj.firstObject as! [[AnyObject]])
+                onCompletion(arr[0][0] as? String ?? self)
+            }catch{
+                onCompletion(self)
+            }
+        })
+        task.resume()
+    }
+}
